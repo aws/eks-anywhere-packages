39c43d29048ed0b054b62820a87227daeb07f78e
Terry Howe <tlhowe@amazon.com>
2022-03-01 09:03:26 -0700

    Update test signing

--- pkg/signature/testdata/sign_file.sh
+++ pkg/signature/testdata/sign_file.sh
@@ -9,6 +9,15 @@ file=$1
 alwaysexcludes='.metadata.annotations."eksa.aws.com/signature"'
 has_excludes=$(yq "${file}" '.metadata.annotations."eksa.aws.com/excludes"')
 
+function debaser64 {
+  if [ "$(uname -s)" == "Darwin" ]
+  then
+    base64 | tr -d '\n'
+  else
+    base64 -w0
+  fi
+}
+
 excludes=""
 if [ "${has_excludes}" != "null" ]; then
     excludes=$(yq "${file}" '.metadata.annotations."eksa.aws.com/excludes"' | base64 -d | cat <(echo ${alwaysexcludes}) - | paste -sd "," -) 
@@ -16,6 +25,6 @@ fi
 fixed=$(yq ${file} \
     "del(${alwaysexcludes}$([ ! -z ${excludes} ] && echo , ${excludes})) | walk( if type == \"object\" then with_entries(select(.value != \"\" and .value != null and .value != [])) else . end)" "--indentless-lists -Y -S")
 digest=$(openssl dgst -sha256 -binary <<<"${fixed}")
-signature=$(openssl pkeyutl -inkey pkg/signature/testdata/private.ec.key -sign -in <(echo "${digest}") | base64 -w0)
+signature=$(openssl pkeyutl -inkey pkg/signature/testdata/private.ec.key -sign -in <(echo "${digest}") | debaser64)
 yq "${file}" ".metadata.annotations.\"eksa.aws.com/signature\" = \"${signature}\"" -Y > "${file}.signed"
-echo -n "${digest}" | base64 -w0 > ${file}.digest
+echo -n "${digest}" | debaser64 > ${file}.digest
