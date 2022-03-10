073cdfee2dd06e21c2b7d2ce3f6b7a302ae69d00
Eric Wollesen <169516+ewollesen@users.noreply.github.com>
2022-03-10 11:27:55 -0700

    Updates bundle puller for new oras (#27)
    When our oras was updated, this got missed. Also, we don't need the docker
    stuff anymore, oras is enough on its own.

--- pkg/artifacts/registry.go
+++ pkg/artifacts/registry.go
@@ -3,20 +3,14 @@ package artifacts
 import (
 	"context"
 	"fmt"
-	"os"
 
-	"github.com/containerd/containerd/remotes"
-	"github.com/containerd/containerd/remotes/docker"
 	"oras.land/oras-go/pkg/content"
 	"oras.land/oras-go/pkg/oras"
 )
 
 // RegistryPuller handles pulling OCI artifacts from an OCI registry
 // (i.e. bundles)
-type RegistryPuller struct {
-	resolver remotes.Resolver
-	store    *content.Memory
-}
+type RegistryPuller struct{}
 
 var _ Puller = (*RegistryPuller)(nil)
 
@@ -25,26 +19,22 @@ var _ Puller = (*RegistryPuller)(nil)
 // It assumes AWS ECR, and uses a password that exists in the ECR_PASSWORD
 // environment variable.
 func NewRegistryPuller() *RegistryPuller {
-	authFn := func(hostname string) (string, string, error) {
-		return "AWS", os.Getenv("ECR_PASSWORD"), nil
-	}
-	authorizer := docker.NewDockerAuthorizer(docker.WithAuthCreds(authFn))
-
-	return &RegistryPuller{
-		resolver: docker.NewResolver(docker.ResolverOptions{
-			Hosts: docker.ConfigureDefaultRegistries(docker.WithAuthorizer(authorizer)),
-		}),
-		store: content.NewMemory(),
-	}
+	return &RegistryPuller{}
 }
 
 func (p *RegistryPuller) Pull(ctx context.Context, ref string) ([]byte, error) {
-	_, err := oras.Copy(ctx, p.resolver, ref, p.store, "")
+	registry, err := content.NewRegistry(content.RegistryOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("creating registry: %w", err)
+	}
+	store := content.NewMemory()
+
+	_, err = oras.Copy(ctx, registry, ref, store, "")
 	if err != nil {
 		return nil, fmt.Errorf("pulling artifact %q: %s", ref, err)
 	}
 
-	_, data, ok := p.store.GetByName("bundle.yaml")
+	_, data, ok := store.GetByName("bundle.yaml")
 	if !ok {
 		return nil, fmt.Errorf("getting bundle data: unknown")
 	}
