2460faa4a32612fb59c5691681f5a0f852cdfa52
Terry Howe <terrylhowe@gmail.com>
2022-03-17 08:56:03 -0600

    Update helm crds (#38)

--- hack/helm.sh
+++ hack/helm.sh
@@ -1,12 +1,23 @@
-#!/bin/bash -e
+#!/usr/bin/env bash
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-chart() {
-    (
-        cd charts
-        helm lint eks-anywhere-packages
-        helm package eks-anywhere-packages
-        helm-docs
-    )
-}
+SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+cd ${SCRIPT_ROOT}/..
 
-chart
+cat config/crd/bases/packages.eks.amazonaws.com_package* >charts/eks-anywhere-packages/templates/crd.yaml
+cd charts
+helm lint eks-anywhere-packages
+helm package eks-anywhere-packages
+helm-docs
