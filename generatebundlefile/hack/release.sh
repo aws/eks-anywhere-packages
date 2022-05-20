7907a28c772f198b52f79543a2473237dc4ea661
Eric Wollesen <169516+ewollesen@users.noreply.github.com>
2022-05-20 05:30:28 -0600

    Don't leak credentials via process table inspection. (#210)
    When credentials are part of a command's arguments, like these were,
    they're exposed via process table inspection (e.g. via `ps`). In
    addition, they could possibly then be captured in logs.
    
    The replacement defines a function that wraps the oras call and passes
    in the credentials on stdin, removing them from the process table and
    preventing exposure via logs.

--- generatebundlefile/hack/release.sh
+++ generatebundlefile/hack/release.sh
@@ -52,10 +52,29 @@ ${BASE_DIRECTORY}/generatebundlefile/bin/generatebundlefile  \
 
 make oras-install
 
-ECR_PASSWORD=$(aws ecr-public get-login-password --region us-east-1 --profile ${PROFILE})
+# orasAuth wraps calls to oras and sends credentials on stdin.
+#
+# Use it with any oras sub-command that supports the --password-stdin
+# flag. The function will detect if pushing to public and adjust
+# accordingly. Note that the caller is still responsible for all flags
+# sent to oras (including --password-stdin!)
+function orasAuth () {
+    declare -a cmd=( ecr )
+
+    for arg in "$@"; do
+	if [[ "$arg" =~ public\.ecr\.aws ]]; then
+	    cmd=( ecr-public --region us-east-1 )
+	    break
+	fi
+    done
+
+    aws ${cmd[*]} get-login-password --profile ${PROFILE:-default} \
+	| ${BASE_DIRECTORY}/bin/oras "$*"
+}
+
 cd ${BASE_DIRECTORY}/generatebundlefile/output
-${BASE_DIRECTORY}/bin/oras push -u AWS -p "${ECR_PASSWORD}" "${IMAGE_REGISTRY}/eks-anywhere-packages-bundles:v1-21-${CODEBUILD_BUILD_NUMBER}" bundle.yaml
-${BASE_DIRECTORY}/bin/oras push -u AWS -p "${ECR_PASSWORD}" "${IMAGE_REGISTRY}/eks-anywhere-packages-bundles:v1-21-latest" bundle.yaml
+orasAuth push --username AWS --password-stdin "${IMAGE_REGISTRY}/eks-anywhere-packages-bundles:v1-21-${CODEBUILD_BUILD_NUMBER}" bundle.yaml
+orasAuth push --username AWS --password-stdin "${IMAGE_REGISTRY}/eks-anywhere-packages-bundles:v1-21-latest" bundle.yaml
 
 # 1.22 Bundle Build
 cd ${BASE_DIRECTORY}/generatebundlefile
@@ -65,5 +84,5 @@ ${BASE_DIRECTORY}/generatebundlefile/bin/generatebundlefile  \
     --key alias/${KMS_KEY}
 
 cd ${BASE_DIRECTORY}/generatebundlefile/output
-${BASE_DIRECTORY}/bin/oras push -u AWS -p "${ECR_PASSWORD}" "${IMAGE_REGISTRY}/eks-anywhere-packages-bundles:v1-22-${CODEBUILD_BUILD_NUMBER}" bundle.yaml
-${BASE_DIRECTORY}/bin/oras push -u AWS -p "${ECR_PASSWORD}" "${IMAGE_REGISTRY}/eks-anywhere-packages-bundles:v1-22-latest" bundle.yaml
+orasAuth push --username AWS --password-stdin "${IMAGE_REGISTRY}/eks-anywhere-packages-bundles:v1-22-${CODEBUILD_BUILD_NUMBER}" bundle.yaml
+orasAuth push --username AWS --password-stdin "${IMAGE_REGISTRY}/eks-anywhere-packages-bundles:v1-22-latest" bundle.yaml
