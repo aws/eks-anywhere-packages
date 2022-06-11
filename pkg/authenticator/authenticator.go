cec1509367a88dd9291c4316fa68d192730fe9a4
Jun Shun Zhang <junshun@amazon.com>
2022-06-10 20:15:44 -0400

    Adding ability for helm driver to pull helm charts from private repository using os enviroment variable that will be passed in via the helm chart

---
+++ pkg/authenticator/authenticator.go
@@ -0,0 +1,19 @@
+package authenticator
+
+// DockerAuth Structure for the authentication file
+type DockerAuth struct {
+	Auths map[string]DockerAuthRegistry `json:"auths,omitempty"`
+}
+
+type DockerAuthRegistry struct {
+	Auth string `json:"auth"`
+}
+
+// Authenticator is an interface for creating an authentication file with credentials to private registries
+//
+// Currently this is used with the Helm Driver which takes credentials in this way
+// For this first implementation, kubernetes secrets will be used to pass in a token
+type Authenticator interface {
+	// GetAuthFile Gets Authentication File Path for OCI Registry
+	GetAuthFile() (string, error)
+}
---
+++ pkg/authenticator/dockersecret.go
@@ -0,0 +1,45 @@
+package authenticator
+
+import (
+	"fmt"
+	"os"
+)
+
+type dockersecret struct {
+}
+
+var _ Authenticator = (*dockersecret)(nil)
+
+func NewDockerSecret() *dockersecret {
+	return &dockersecret{}
+}
+
+func (s *dockersecret) GetAuthFile() (string, error) {
+	token, err := s.getSecretToken()
+	if err != nil {
+		return "", fmt.Errorf("Failed to get authfile %s\n", err)
+	}
+	authfile, err := createAuthFile(token)
+	if err != nil {
+		return "", fmt.Errorf("Failed to get authfile %s\n", err)
+	}
+
+	return authfile, nil
+}
+
+func (s *dockersecret) getSecretToken() (string, error) {
+	// TODO Handle encryption here if secret is encrypted
+	dockerconfig := os.Getenv("DOCKER_CONFIG")
+
+	return dockerconfig, nil
+}
+
+func createAuthFile(data string) (string, error) {
+	f, err := os.CreateTemp("", "dockerAuth")
+	if err != nil {
+		return "", fmt.Errorf("Creating tempfile %w", err)
+	}
+	defer f.Close()
+	fmt.Fprint(f, data)
+	return f.Name(), nil
+}
