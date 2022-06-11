83eb3a876cde1eff0d396eced04ccf5047016935
Terry Howe <tlhowe@amazon.com>
2022-06-11 10:19:46 -0600

    Use registry from PackageBundleController

--- pkg/bundle/client.go
+++ pkg/bundle/client.go
@@ -78,6 +78,17 @@ func (bc *bundleClient) GetActiveBundle(ctx context.Context) (activeBundle *api.
 	if err != nil {
 		return nil, err
 	}
+
+	for i, bundlePackage := range activeBundle.Spec.Packages {
+		if len(bundlePackage.Source.Registry) < 1 {
+			if len(pbc.Spec.Source.Registry) < 1 {
+				activeBundle.Spec.Packages[i].Source.Registry = "public.ecr.aws/eks-anywhere"
+			} else {
+				activeBundle.Spec.Packages[i].Source.Registry = pbc.Spec.Source.Registry
+			}
+		}
+	}
+
 	return activeBundle, nil
 }
 
