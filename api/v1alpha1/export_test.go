2216023100b419069af634e7d1b3a89d7f80d0fb
Terry Howe <tlhowe@amazon.com>
2022-08-04 14:16:35 -0600

    Remove export test

--- api/v1alpha1/export_test.go
+++
@@ -1,5 +0,0 @@
-// Export private functions and methods for testing
-package v1alpha1
-
-var GetMajorMinorBuild = (*PackageBundle).getMajorMinorBuild
-var GetMajorMinorFromString = getMajorMinorFromString
