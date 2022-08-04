e6f07946b0f7e7bcae948a928698c7172892dfa4
a-cool-train <98445475+a-cool-train@users.noreply.github.com>
2022-08-04 09:28:27 -0700

    Refactor validation (#337)
    * Design for restructuring github repo
    
    * Refactor validator
    
    * Remove unnecessary files
    
    * Remove unnecessary methods and merge with main

--- pkg/webhook/package_webhook.go
+++ pkg/webhook/package_webhook.go
@@ -16,11 +16,8 @@ package webhook
 
 import (
 	"bytes"
-	"compress/gzip"
 	"context"
-	"encoding/base64"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/xeipuuv/gojsonschema"
@@ -86,7 +83,7 @@ func (v *packageValidator) Handle(ctx context.Context, request admission.Request
 }
 
 func (v *packageValidator) isPackageConfigValid(p *v1alpha1.Package, activeBundle *v1alpha1.PackageBundle) (bool, error) {
-	packageInBundle, err := getPackageInBundle(activeBundle, p.Spec.PackageName)
+	packageInBundle, err := activeBundle.GetPackageFromBundle(p.Spec.PackageName)
 	if err != nil {
 		return false, err
 	}
@@ -96,7 +93,7 @@ func (v *packageValidator) isPackageConfigValid(p *v1alpha1.Package, activeBundl
 		return false, fmt.Errorf("package %s does not contain any versions", p.Name)
 	}
 
-	jsonSchema, err := getPackagesJsonSchema(packageInBundle)
+	jsonSchema, err := packageInBundle.GetJsonSchema()
 	if err != nil {
 		return false, err
 	}
@@ -132,38 +129,6 @@ func validatePackage(p *v1alpha1.Package, jsonSchema []byte) (*gojsonschema.Resu
 	configToValidate := gojsonschema.NewStringLoader(string(packageConfig))
 
 	return schema.Validate(configToValidate)
-}
-
-func getPackagesJsonSchema(bundlePackage *v1alpha1.BundlePackage) ([]byte, error) {
-	// The package configuration is gzipped and base64 encoded
-	// When processing the configuration, the reverse occurs: base64 decode, then unzip
-	configuration := bundlePackage.Source.Versions[0].Schema
-	decodedConfiguration, err := base64.StdEncoding.DecodeString(configuration)
-	if err != nil {
-		return nil, fmt.Errorf("error decoding configurations %v", err)
-	}
-
-	reader := bytes.NewReader(decodedConfiguration)
-	gzreader, err := gzip.NewReader(reader)
-	if err != nil {
-		return nil, fmt.Errorf("error when uncompressing configurations %v", err)
-	}
-
-	output, err := ioutil.ReadAll(gzreader)
-	if err != nil {
-		return nil, fmt.Errorf("error reading configurations %v", err)
-	}
-
-	return output, nil
-}
-
-func getPackageInBundle(activeBundle *v1alpha1.PackageBundle, packageName string) (*v1alpha1.BundlePackage, error) {
-	for _, p := range activeBundle.Spec.Packages {
-		if p.Name == packageName {
-			return &p, nil
-		}
-	}
-	return nil, fmt.Errorf("package %s not found", packageName)
 }
 
 // InjectDecoder injects the decoder.
