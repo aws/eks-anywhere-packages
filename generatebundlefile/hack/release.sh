16872addb4ece6c0f23711a74faedec9f8203b7d
jonahjon <jonahjones094@gmail.com>
2022-09-02 11:19:19 -0500

    fixing image copy source file
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- generatebundlefile/hack/release.sh
+++ generatebundlefile/hack/release.sh
@@ -42,7 +42,7 @@ aws ecr get-login-password --region us-west-2 | HELM_EXPERIMENTAL_OCI=1 helm reg
 aws ecr-public get-login-password --region us-east-1 | HELM_EXPERIMENTAL_OCI=1 helm registry login --username AWS --password-stdin public.ecr.aws
 
 ${BASE_DIRECTORY}/generatebundlefile/bin/generatebundlefile  \
-    --input ${BASE_DIRECTORY}/generatebundlefile/data/input_121.yaml \
+    --input ${BASE_DIRECTORY}/generatebundlefile/data/input_release.yaml \
     --private-profile ${PROFILE}
 
 # Release Helm Chart, and bundle to Production account
