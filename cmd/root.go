eada648f5077d60613451a5c26bbc0203b1c51d4
Terry Howe <terrylhowe@gmail.com>
2022-11-30 10:17:02 -0700

    Bring in Cobra and Viper (#655)
    * Migrate packages from eksa-packages
    
    * Bring in cobra and viper
    
    * Fix lint issues
    
    * Fix main.go lint issues

---
+++ cmd/root.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"context"
+	"log"
+
+	"github.com/go-logr/logr"
+	"github.com/go-logr/zapr"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var packageLog logr.Logger
+
+var rootCmd = &cobra.Command{
+	Use:              "package-manager",
+	Short:            "Amazon EKS Anywhere Package Manager",
+	Long:             "Manage Kubernetes packages with EKS Anywhere Curated Packages",
+	PersistentPreRun: rootPersistentPreRun,
+}
+
+func init() {
+	rootCmd.PersistentFlags().IntP("verbosity", "v", 0, "Set the log level verbosity")
+	if err := viper.BindPFlags(rootCmd.PersistentFlags()); err != nil {
+		log.Fatalf("failed to bind flags for root: %v", err)
+	}
+}
+
+func rootPersistentPreRun(_ *cobra.Command, _ []string) {
+	level := viper.GetInt("verbosity")
+	cfg := zap.NewDevelopmentConfig()
+	cfg.Level = zap.NewAtomicLevelAt(zapcore.Level(-1 * level))
+	cfg.EncoderConfig.EncodeLevel = nil
+	cfg.DisableCaller = true
+	cfg.DisableStacktrace = false
+	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	zapLog, err := cfg.Build()
+	if err != nil {
+		log.Fatalf("Error initializing logging: %v", err)
+	}
+	packageLog = zapr.NewLogger(zapLog)
+}
+
+func Execute() error {
+	return rootCmd.ExecuteContext(context.Background())
+}
--- main.go
+++ main.go
@@ -15,107 +15,15 @@
 package main
 
 import (
-	"flag"
 	"os"
 
-	"go.uber.org/zap/zapcore"
-	"k8s.io/apimachinery/pkg/runtime"
-	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-
-	api "github.com/aws/eks-anywhere-packages/api/v1alpha1"
-	"github.com/aws/eks-anywhere-packages/controllers"
-	"github.com/aws/eks-anywhere-packages/pkg/webhook"
+	"github.com/aws/eks-anywhere-packages/cmd"
 )
-
-var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
-)
-
-func init() {
-	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-
-	utilruntime.Must(api.AddToScheme(scheme))
-	//+kubebuilder:scaffold:scheme
-}
 
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-		TimeEncoder: zapcore.ISO8601TimeEncoder,
-	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
-
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
-
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "6ef7a950.eks.amazonaws.com",
-	})
+	err := cmd.Execute()
 	if err != nil {
-		setupLog.Error(err, "unable to start manager")
-		os.Exit(1)
+		os.Exit(-1)
 	}
-
-	if err = controllers.RegisterPackageBundleReconciler(mgr); err != nil {
-		setupLog.Error(err, "unable to register controller", "controller", "PackageBundle")
-		os.Exit(1)
-	}
-	if err = controllers.RegisterPackageBundleControllerReconciler(mgr); err != nil {
-		setupLog.Error(err, "unable to register controller", "controller", "PackageBundleController")
-		os.Exit(1)
-	}
-	if err = controllers.RegisterPackageReconciler(mgr); err != nil {
-		setupLog.Error(err, "unable to register controller", "controller", "Package")
-		os.Exit(1)
-	}
-
-	if os.Getenv("ENABLE_WEBHOOKS") == "true" {
-		if err := webhook.InitPackageBundleValidator(mgr); err != nil {
-			setupLog.Error(err, "unable to create webhook", "webhook", "PackageBundle")
-			os.Exit(1)
-		}
-		if err := webhook.InitPackageBundleControllerValidator(mgr); err != nil {
-			setupLog.Error(err, "unable to create webhook", "webhook", "PackageBundleController")
-			os.Exit(1)
-		}
-		if err = webhook.InitPackageValidator(mgr); err != nil {
-			setupLog.Error(err, "unable to create webhook", "webhook", "Package")
-			os.Exit(1)
-		}
-	}
-	//+kubebuilder:scaffold:builder
-
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up health check")
-		os.Exit(1)
-	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up ready check")
-		os.Exit(1)
-	}
-
-	setupLog.Info("starting manager")
-	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
-		os.Exit(1)
-	}
+	os.Exit(0)
 }
