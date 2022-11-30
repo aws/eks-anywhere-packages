19f63a3638257934f6b1ba1396ab5e49a6e8b897
jonahjon <jonahjones094@gmail.com>
2022-11-30 16:35:43 -0600

    bundle diff check prod
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- generatebundlefile/hack/release_prod.sh
+++ generatebundlefile/hack/release_prod.sh
@@ -84,8 +84,15 @@ function push () {
     local version=${1?:no version specified}
     cd "${BASE_DIRECTORY}/generatebundlefile/output-${version}"
     awsAuth "$REPO" | "$ORAS_BIN" login "$REPO" --username AWS --password-stdin
-    "$ORAS_BIN" push "${REPO}:v${version}-${CODEBUILD_BUILD_NUMBER}" bundle.yaml
-    "$ORAS_BIN" push "${REPO}:v${version}-latest" bundle.yaml
+    "$ORAS_BIN" pull "${REPO}:v${version}-latest" -o ${version}
+    removeBundleMetadata ${version}/bundle.yaml
+    removeBundleMetadata bundle.yaml
+    if (git diff --no-index --quiet -- ${version}/bundle.yaml.stripped bundle.yaml.stripped) then
+        echo "bundle contents are identical skipping bundle push for ${version}"
+    else
+        "$ORAS_BIN" push "${REPO}:v${version}-${CODEBUILD_BUILD_NUMBER}" bundle.yaml
+        "$ORAS_BIN" push "${REPO}:v${version}-latest" bundle.yaml
+    fi
 }
 
 for version in 1-20 1-21 1-22 1-23 1-24; do
