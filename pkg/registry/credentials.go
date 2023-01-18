de9c0be404de5efa2210fef292c95c516e149e4a
Eric Wollesen <169516+ewollesen@users.noreply.github.com>
2023-01-17 17:29:36 -0700

    Lift configfile (#757)
    * Don't store the RegistryPuller's StorageClient
    
    There's no point in storing it, as it's never reused.
    
    * Lift configfile.ConfigFile out of CredentialsStore
    
    Rather than assuming that users of CredentialStore will know to call Init
    after instantiation, the CredentialStore's dependency on a Docker *ConfigFile
    is made explicit by requiring it to be passed to the constructor. Now we need
    never worry that a user will forget to call Init, and it removes the awkward
    pattern of New-then-Init.

--- pkg/registry/credentials.go
+++ pkg/registry/credentials.go
@@ -1,44 +1,28 @@
 package registry
 
 import (
-	"github.com/docker/cli/cli/config"
 	"github.com/docker/cli/cli/config/configfile"
 	"github.com/docker/cli/cli/config/credentials"
 	"oras.land/oras-go/v2/registry/remote/auth"
 )
 
-// CredentialStore for registry credentials such as ~/.docker/config.json.
-type CredentialStore struct {
-	directory  string
+// DockerCredentialStore for Docker registry credentials, like ~/.docker/config.json.
+type DockerCredentialStore struct {
 	configFile *configfile.ConfigFile
 }
 
-// NewCredentialStore create a credential store.
-func NewCredentialStore() *CredentialStore {
-	return &CredentialStore{
-		directory: config.Dir(),
+// NewDockerCredentialStore creates a DockerCredentialStore.
+func NewDockerCredentialStore(configFile *configfile.ConfigFile) *DockerCredentialStore {
+	if !configFile.ContainsAuth() {
+		configFile.CredentialsStore = credentials.DetectDefaultStore(configFile.CredentialsStore)
 	}
-}
-
-// SetDirectory override default directory.
-func (cs *CredentialStore) SetDirectory(directory string) {
-	cs.directory = directory
-}
-
-// Init initialize a credential store.
-func (cs *CredentialStore) Init() (err error) {
-	cs.configFile, err = config.Load(cs.directory)
-	if err != nil {
-		return err
+	return &DockerCredentialStore{
+		configFile: configFile,
 	}
-	if !cs.configFile.ContainsAuth() {
-		cs.configFile.CredentialsStore = credentials.DetectDefaultStore(cs.configFile.CredentialsStore)
-	}
-	return nil
 }
 
 // Credential get an authentication credential for a given registry.
-func (cs *CredentialStore) Credential(registry string) (auth.Credential, error) {
+func (cs *DockerCredentialStore) Credential(registry string) (auth.Credential, error) {
 	authConf, err := cs.configFile.GetCredentialsStore(registry).Get(registry)
 	if err != nil {
 		return auth.EmptyCredential, err
