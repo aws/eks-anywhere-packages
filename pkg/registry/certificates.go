85fe323a5f647ffd9d91538c7dd3a6a8424e1ef0
Terry Howe <tlhowe@amazon.com>
2023-01-30 08:41:50 -0700

    Support cluster specific ca.crt and insecure

--- pkg/registry/certificates.go
+++ pkg/registry/certificates.go
@@ -4,8 +4,43 @@ import (
 	"crypto/x509"
 	"fmt"
 	"os"
+	"path"
 	"path/filepath"
 )
+
+const (
+	registryConfigPath = "/tmp/config/registry"
+	certFile           = "ca.crt"
+	insecureFile       = "insecure"
+)
+
+func GetManagementClusterCertificate() (certificates *x509.CertPool, err error) {
+	return GetClusterCertificate(os.Getenv("CLUSTER_NAME"))
+}
+
+func GetRegistryInsecure(clusterName string) bool {
+	caFile := path.Join(registryConfigPath, clusterName, insecureFile)
+	if _, err := os.Stat(caFile); err != nil {
+		return false
+	}
+	return true
+}
+
+func GetClusterCertificateFileName(clusterName string) string {
+	caFile := path.Join(registryConfigPath, clusterName, certFile)
+	if _, err := os.Stat(caFile); err != nil {
+		return ""
+	}
+	return caFile
+}
+
+func GetClusterCertificate(clusterName string) (certificates *x509.CertPool, err error) {
+	caFile := GetClusterCertificateFileName(clusterName)
+	if caFile == "" {
+		return nil, nil
+	}
+	return GetCertificates(caFile)
+}
 
 // GetCertificates get X509 certificates.
 func GetCertificates(certFile string) (certificates *x509.CertPool, err error) {
