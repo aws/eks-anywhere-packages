c461f0f76df0fd5692419db0e5bb75abb4788344
jonahjon <jonahjones094@gmail.com>
2023-06-19 14:24:40 -0500

    changing api version for double cloud provider version
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- credentialproviderpackage/pkg/configurator/linux/linux_test.go
+++ credentialproviderpackage/pkg/configurator/linux/linux_test.go
@@ -114,24 +114,6 @@ func Test_linuxOS_updateKubeletArguments(t *testing.T) {
 			args:             args{line: ""},
 			outputConfigPath: dir + "/" + credProviderFile,
 			configWantPath:   "testdata/expected-config-alpha.yaml",
-			k8sVersion:       "v1.23",
-			want: fmt.Sprintf(" --feature-gates=KubeletCredentialProviders=true "+
-				"--image-credential-provider-config=%s%s", dir, credProviderFile),
-		},
-		{
-			name: "test beta api",
-			fields: fields{
-				profile:       "eksa-packages",
-				extraArgsPath: dir,
-				basePath:      dir,
-				config: constants.CredentialProviderConfigOptions{
-					ImagePatterns:        []string{constants.DefaultImagePattern},
-					DefaultCacheDuration: constants.DefaultCacheDuration,
-				},
-			},
-			args:             args{line: ""},
-			outputConfigPath: dir + "/" + credProviderFile,
-			configWantPath:   "testdata/expected-config-beta.yaml",
 			k8sVersion:       "v1.25",
 			want: fmt.Sprintf(" --feature-gates=KubeletCredentialProviders=true "+
 				"--image-credential-provider-config=%s%s", dir, credProviderFile),
