039335efd317cf1d162ab26f82cf0f896aae9446
Abhay Krishna <arnchlm@amazon.com>
2023-10-25 17:56:16 -0700

    Remove 1-22 and 1-23 references from bundle generation scripts (#1009)

--- generatebundlefile/hack/release_staging.sh
+++ generatebundlefile/hack/release_staging.sh
@@ -86,19 +86,19 @@ fi
 # Generate Bundles from Public ECR
 export AWS_PROFILE=staging
 export AWS_CONFIG_FILE=${BASE_DIRECTORY}/generatebundlefile/stagingconfigfile
-for version in 1-22 1-23 1-24 1-25 1-26 1-27 1-28; do
+for version in 1-24 1-25 1-26 1-27 1-28; do
     generate ${version} "staging"
 done
 
 # Push Bundles to Public ECR
 aws ecr-public get-login-password --region us-east-1 | HELM_EXPERIMENTAL_OCI=1 helm registry login --username AWS --password-stdin public.ecr.aws
-for version in 1-22 1-23 1-24 1-25 1-26 1-27 1-28; do
+for version in 1-24 1-25 1-26 1-27 1-28; do
     push ${version}
 done
 
 # Check images from Bundle in Private ECR
 export AWS_CONFIG_FILE=${BASE_DIRECTORY}/generatebundlefile/configfile
 export AWS_PROFILE=packages
-for version in 1-22 1-23 1-24 1-25 1-26 1-27 1-28; do
+for version in 1-24 1-25 1-26 1-27 1-28; do
     regionCheck ${version}
 done
