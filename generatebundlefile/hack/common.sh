48ee92a2f89ecb0f544d182cae24a3d068bf1d26
Abhay Krishna <arnchlm@amazon.com>
2024-02-27 23:19:36 -0800

    Fix AWS profile for release jobs (#1074)

--- generatebundlefile/hack/common.sh
+++ generatebundlefile/hack/common.sh
@@ -29,7 +29,15 @@ function orasLogin () {
         region="--region=us-east-1"
     fi
 
-    aws "$awsCmd" "$region" --profile=${PROFILE:-} get-login-password | "$ORAS_BIN" login "$repo" --username AWS --password-stdin
+    regional_build_mode=${REGIONAL_BUILD_MODE:-}
+    if [[ "$regional_build_mode" == "true" ]]; then
+        profile=default
+    else
+        profile=${PROFILE:-}
+    fi
+    export AWS_PROFILE=$profile
+
+    aws "$awsCmd" "$region" --profile=$profile get-login-password | "$ORAS_BIN" login "$repo" --username AWS --password-stdin
 }
 
 function generate () {
