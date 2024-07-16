23fe2b8aee1ef7300e453aadeece5123635f48e6
Abhay Krishna <arnchlm@amazon.com>
2024-07-15 23:24:43 -0700

    Tag package bundles differently for regional staging/prod (#1134)

--- generatebundlefile/hack/release_staging.sh
+++ generatebundlefile/hack/release_staging.sh
@@ -104,7 +104,7 @@ done
 # Push Bundles to Public ECR
 aws ecr-public get-login-password --region us-east-1 | HELM_EXPERIMENTAL_OCI=1 helm registry login --username AWS --password-stdin public.ecr.aws
 for version in 1-26 1-27 1-28 1-29 1-30; do
-    push ${version}
+    push ${version} "staging"
 done
 
 # Check images from Bundle in Private ECR
